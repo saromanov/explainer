32343befa6b06413db0dc6c7928170bf445bcdb6
Sergey <xxsmotur@gmail.com>
2019-12-09 23:55:06 +0500

    init

---
+++ setup.py
@@ -0,0 +1,58 @@
+import io
+import re
+
+from setuptools import find_packages
+from setuptools import setup
+
+with io.open("README.md", "rt", encoding="utf8") as f:
+    readme = f.read()
+
+with io.open("src/explainer/__init__.py", "rt", encoding="utf8") as f:
+    version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
+
+setup(
+    name="Flask",
+    version=version,
+    url="https://github.com/saromanov/explainer",
+    license="MIT",
+    author="Sergey Romanov",
+    author_email="xxsmotur@gmail.com",
+    description="Writing results after execution of EXPLAIN command on PostgreSQL",
+    long_description=readme,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Flask",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
+    install_requires=[
+        "Werkzeug>=0.15",
+        "Jinja2>=2.10.1",
+        "itsdangerous>=0.24",
+        "click>=5.1",
+    ],
+    extras_require={
+        "dotenv": ["python-dotenv"],
+        "dev": [
+            "pytest",
+            "coverage",
+            "tox",
+        ],
+    },
+)
