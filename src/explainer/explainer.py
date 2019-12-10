9c33106e82cec026cd9e861cff889e7111642c27
Sergey <xxsmotur@gmail.com>
2019-12-11 00:33:16 +0500

    Add method for get sort names

--- src/explainer/explainer.py
+++ src/explainer/explainer.py
@@ -5,6 +5,7 @@ class Analyzer:
     def __init__(self):
         self.planning_time = ''
         self.execution_time = ''
+        self._sort_names = []
 
 def create_engine(path):
     if path is None:
@@ -21,10 +22,14 @@ def explain(session, query):
 
 def parse_explain(value):
     an = Analyzer()
-    check_ms = lambda x, y: x.startswith(y)
+    check_start = lambda x, y: x.startswith(y)
+    sort_names = lambda x: x.split(',')
     for v in value:
         data = v[0]
-        if check_ms(data, 'Planning'):
+        if check_start(data, 'Planning'):
             an.planning_time = data.split(':')[1]
-        if check_ms(data, 'Execution'):
+        if check_start(data, 'Execution'):
             an.execution_time = data.split(':')[1]
+        if check_start(data, 'Sort Key'):
+            an.sort_names = sort_names(data)
+        print(data)
