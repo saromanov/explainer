faf2dbe1c62a6227f6dcebc764dd42738839af57
Sergey <xxsmotur@gmail.com>
2019-12-11 00:22:58 +0500

    Add analyzer object

--- src/explainer/explainer.py
+++ src/explainer/explainer.py
@@ -1,6 +1,11 @@
 import sqlalchemy as db
 from exceptions import CreateEngineException, NotCompletedOutputException
 
+class Analyzer:
+    def __init__(self):
+        self.planning_time = ''
+        self.execution_time = ''
+
 def create_engine(path):
     if path is None:
         raise CreateEngineException('path for engine is empty')
@@ -15,5 +20,11 @@ def explain(session, query):
     return session.execute('EXPLAIN ANALYZE {0}'.format(query)).fetchall()
 
 def parse_explain(value):
+    an = Analyzer()
+    check_ms = lambda x, y: x.startswith(y)
     for v in value:
-        print(v)
+        data = v[0]
+        if check_ms(data, 'Planning'):
+            an.planning_time = data.split(':')[1]
+        if check_ms(data, 'Execution'):
+            an.execution_time = data.split(':')[1]
