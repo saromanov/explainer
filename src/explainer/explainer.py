87c99abf06ed932952015c4d9e97c29c29ba8d5f
Sergey <xxsmotur@gmail.com>
2020-02-13 02:12:20 +0500

    Add lambda for getting data

--- src/explainer/explainer.py
+++ src/explainer/explainer.py
@@ -24,12 +24,13 @@ def parse_explain(value):
     an = Analyzer()
     check_start = lambda x, y: x.startswith(y)
     sort_names = lambda x: x.split(',')
+    get_time = lambda x: x.split(':')[1]
     for v in value:
         data = v[0]
         if check_start(data, 'Planning'):
-            an.planning_time = data.split(':')[1]
+            an.planning_time = get_time(data)
         if check_start(data, 'Execution'):
-            an.execution_time = data.split(':')[1]
+            an.execution_time = get_time(data)
         if check_start(data, 'Sort Key'):
             an.sort_names = sort_names(data)
         print(data)
