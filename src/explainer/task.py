679ad84d77a2827fd2dde96264cb9aa7b0cbb18f
Sergey <xxsmotur@gmail.com>
2020-02-22 11:54:04 +0500

    Draft for running task on parse explain

--- src/explainer/task.py
+++ src/explainer/task.py
@@ -1,3 +1,6 @@
+from parse import parse_explaing
+from db import explain
+
 class Task:
     ''' defines query as a task
     '''
@@ -5,4 +8,12 @@ class Task:
         self._query = query
         self._rows = 0
         self._times = times
+    
+    def run(self):
+        for x in range(self._times):
+            parse_explain(explain(query))
+    
+    
+    
+
     
